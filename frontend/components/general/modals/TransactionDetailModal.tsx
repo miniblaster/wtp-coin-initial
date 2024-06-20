1590b66df55c7cc0ff1b9f427f099af35c809c19
miniblaster <minionboss0626@gmail.com>
2024-06-20 13:12:09 -0400

    feat: Implement whole feature of TransactionDetailModal component

--- frontend/components/general/modals/TransactionDetailModal.tsx
+++ frontend/components/general/modals/TransactionDetailModal.tsx
@@ -1,8 +1,12 @@
 import Image from "next/image";
 
+import classNames from "classnames";
 import { Flex, Modal, Typography, Button } from "antd";
 
+import SendIconSVG from "@/components/svg/SendIconSVG";
 import RedFlag from "@/public/images/general/RedFlag.svg";
+import ContentCopySVG from "@/components/svg/ContentCopySVG";
+import ReceiveIconSVG from "@/components/svg/ReceiveIconSVG";
 
 interface ITransactionDetailModalProps {
   isTransactionDetailModalOpened?: boolean;
@@ -15,6 +19,8 @@ export default function TransactionDetailModal({
   setIsTransactionDetailModalOpened,
   transactionDetailData,
 }: ITransactionDetailModalProps) {
+  let exchangeRate = 1.2;
+
   const handleOk = () => {
     setIsTransactionDetailModalOpened(false);
   };
@@ -25,7 +31,7 @@ export default function TransactionDetailModal({
 
   return (
     <Modal
-      className="flex-col w-full px-[8px]"
+      className="flex-col w-full px-[8px] max-w-[460px]"
       title={<Typography className="p-3 text-[14px]/[22px] font-semibold">Transaction Details</Typography>}
       open={isTransactionDetailModalOpened}
       onOk={handleOk}
@@ -43,7 +49,47 @@ export default function TransactionDetailModal({
       ]}
     >
       <Flex className="flex-col justify-center items-center w-full px-3 py-4 gap-y-3">
-        <Flex>{transactionDetailData.name}</Flex>
+        <Flex className="w-full items-center justify-center py-2 relative">
+          <Flex className="absolute left-4">
+            {transactionDetailData?.amount < 0 ? <SendIconSVG color="#CB1E1E" /> : <ReceiveIconSVG color="#17714A" />}
+          </Flex>
+          <Flex className="flex-col">
+            <Typography
+              className={classNames(
+                "text-center text-[20px]/[30px] font-semibold",
+                transactionDetailData?.amount < 0 ? "text-error" : "text-success"
+              )}
+            >
+              ${Math.abs(transactionDetailData?.amount)}
+            </Typography>
+            <Typography
+              className={classNames(
+                "text-center text-[14px]/[20px] text-error font-medium",
+                transactionDetailData?.amount < 0 ? "text-error" : "text-success"
+              )}
+            >
+              WETP {Math.abs(transactionDetailData?.amount / exchangeRate).toFixed(2)}
+            </Typography>
+          </Flex>
+        </Flex>
+
+        <Flex className="w-full flex-col items-start px-3 py-2">
+          <Typography className="text-disabled text-[12px]/[18px]">Paid to:</Typography>
+          <Typography className="text-[14px]/[22px] font-medium">{transactionDetailData?.name}</Typography>
+        </Flex>
+
+        <Flex className="w-full flex-col items-start px-3 py-2">
+          <Typography className="text-disabled text-[12px]/[18px]">Date:</Typography>
+          <Typography className="text-[14px]/[22px] font-medium">{transactionDetailData?.date}</Typography>
+        </Flex>
+
+        <Flex className="w-full px-3 py-2 items-center justify-between">
+          <Flex className="flex-col gap-y-1">
+            <Typography className="text-disabled text-[12px]/[18px]">Transaction no:</Typography>
+            <Typography className="text-[14px]/[22px] font-medium">23010412432431</Typography>
+          </Flex>
+          <ContentCopySVG color="#637381" />
+        </Flex>
       </Flex>
     </Modal>
   );
